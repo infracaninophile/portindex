73389a13b2c8d55cfdfa2833c7f50b02de48afe9
Matthew Seaman <m.seaman@infracaninophile.co.uk>
2013-12-26 17:01:18 +0000

    Update copyright dates

--- lib/FreeBSD/Portindex/TreeObject.pm
+++ lib/FreeBSD/Portindex/TreeObject.pm
@@ -1,4 +1,4 @@
-# Copyright (c) 2004-2012 Matthew Seaman. All rights reserved.
+# Copyright (c) 2004-2013 Matthew Seaman. All rights reserved.
 #
 # Redistribution and use in source and binary forms, with or without
 # modification, are permitted provided that the following conditions
