9f664319df8af035322a04d0e594be526c8ce1db
Matthew Seaman <m.seaman@infracaninophile.co.uk>
2015-10-18 13:04:35 +0100

    Update release.sh to work with git.  This tags (with PGP signature) the current state of the tree if the module version has been updated, pushes the new tag to the origin, and then does a clean checkout and builds a tarball from the origin.

--- release.sh
+++ release.sh
@@ -11,17 +11,17 @@ export IFS='
 '
 umask 022
 
-WWWREPO="../html/portindex"
+WWWREPO="~/src/html/portindex"
 
-eval $(svn info | awk -F': ' '/^Repository Root:/ { print "SVNROOT="$2 }
-                             /^URL:/ { print "SVNURL="$2 }')
+GIT_ORIGIN=$(git config --get remote.origin.url)
 
-TRUNK=$( echo $SVNURL | sed -e "s,$SVNROOT,," )
-NAME=$(basename $TRUNK)
+NAME=${GIT_ORIGIN##*/}
+NAME=${NAME%.git}
 
 TEMPDIR=$( mktemp -d -t $(basename $0) ) || exit 1
 trap "rm -rf $TEMPDIR; exit" EXIT INT KILL
 
+set -e
 [ -f Makefile ] && make clean
 perl Makefile.PL
 make
@@ -30,19 +30,20 @@ VERSION=$( make -V VERSION )	# eg 3.0
 : ${VERSION:?"Can't determine the release version"}
 
 RELEASETAG="RELEASE_$( echo -n $VERSION | tr -cs 0-9 _ )" # eg RELEASE_3_0
-RELEASEBRANCH="/tags/${RELEASETAG}/${NAME}"
 
-if svn info ^$RELEASEBRANCH >/dev/null 2>&1 ; then
-   echo "$(basename $0): $RELEASEBRANCH already exists"
+if git tag -l | grep -q ^$RELEASETAG ; then
+    echo "$(basename $0): $RELEASETAG already exists"
 else
-   svn copy --parents --message $RELEASETAG ^$TRUNK ^$RELEASEBRANCH || exit 1 
-   echo "$(basename $0): Created release branch $RELEASEBRANCH"
+    git tag -s -f -m "Release $VERSION" $RELEASETAG
+    ##git push --follow-tags
+    echo "$(basename $0): Created release tag $RELEASETAG"
 fi
 
 (
     cd $TEMPDIR
-    svn export "${SVNROOT}${RELEASEBRANCH}" $NAME || exit 1
+    git clone $GIT_ORIGIN $NAME || exit 1
     cd $NAME && \
+	git checkout tags/$RELEASETAG -- && \
 	perl Makefile.PL && \
 	make dist COMPRESS=xz SUFFIX=.xz && \
 	mv -v *.tar.xz ${TMPDIR:-/tmp}
